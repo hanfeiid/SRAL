a842d2872a76f782dc1d83f8e39ded254ae0c62b
Fei Han <hanfeiid@gmail.com>
2016-08-18 10:16:27 -0600

    Update nordland_pr.m

--- nordland_pr.m
+++ nordland_pr.m
@@ -49,7 +49,7 @@ Y = [repmat(class(1,:),n/4,1);
     repmat(class(3,:),n/4,1);
     repmat(class(4,:),n/4,1)];
 
-lambda_G = 0.1;                 % lambda for l-G1 norm 
+lambda_M = 0.1;                 % lambda for l-G1 norm 
 lambda_21 = 0.1;                % lambda for l-2,1 norm
 W = zeros(size(X,1),size(Y,2)); % weight matrix W
 
@@ -74,17 +74,17 @@ for iter_idx = 1 : ITER
     vGist = Igist / (norm(W(dColor+1:dColor+dGist,:),'fro') + eps);
     vHog = Ihog / (norm(W(dColor+dGist+1:dColor+dGist+dHog,:),'fro') + eps);
     vLbp = Ilbp / (norm(W(dColor+dGist+dHog+1:dColor+dGist+dHog+dLbp,:),'fro') + eps);
-    D_G = diag([vColor; vGist; vHog; vLbp]);
+    D_M = diag([vColor; vGist; vHog; vLbp]);
     
     % update W
     for i = 1 : 4
-        W(:,i) = (XX + lambda_G * D_G + lambda_21 * D_21) \ XY(:,i);
+        W(:,i) = (XX + lambda_M * D_M + lambda_21 * D_21) \ XY(:,i);
     end
-    obj_G = norm(W(1:dColor,:),'fro') + norm(W(dColor+1:dColor+dGist,:),'fro') ...
+    obj_M = norm(W(1:dColor,:),'fro') + norm(W(dColor+1:dColor+dGist,:),'fro') ...
             + norm(W(dColor+dGist+1:dColor+dGist+dHog,:),'fro') ...
             + norm(W(dColor+dGist+dHog+1:dColor+dGist+dHog+dLbp,:),'fro') + 4 * eps;
     obj_21 = sum(sqrt(sum(W.*W,2)+eps));
-    obj(iter_idx) = 0.5 * norm(X'*W-Y,'fro')^2 + lambda_G * obj_G + lambda_21 * obj_21;
+    obj(iter_idx) = 0.5 * norm(X'*W-Y,'fro')^2 + lambda_M * obj_M + lambda_21 * obj_21;
     display(sprintf('iteration %d: objective value = %f',iter_idx, obj(iter_idx)));
     
     % end condition
